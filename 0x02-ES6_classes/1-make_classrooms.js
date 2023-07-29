58127346072ea89a7489f2113956c35aa8eeea95
Ronnie Magombedze <mhizhawetswanda@gmail.com>
2023-07-29 14:19:58 +0000

    correction

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,5 @@
 import ClassRoom from './0-classroom';
+
 export default function initializeRooms() {
   const class1 = new ClassRoom(19);
   const class2 = new ClassRoom(20);
@@ -6,4 +7,3 @@ export default function initializeRooms() {
   const newList = [class1, class2, class3];
   return (newList);
 }
-
